0111e2c9548374d700d96604e3b160982286a53b
CHAKlBDEV <chakibthestorm@gmail.com>
2020-02-16 23:07:56 +0100

    Add project files.

---
+++ Work/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.currentRotationCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.endDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rotationTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Le jour ";
+            // 
+            // startDatePicker
+            // 
+            this.startDatePicker.Location = new System.Drawing.Point(81, 12);
+            this.startDatePicker.Name = "startDatePicker";
+            this.startDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDatePicker.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(287, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "rotation";
+            // 
+            // currentRotationCB
+            // 
+            this.currentRotationCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.currentRotationCB.FormattingEnabled = true;
+            this.currentRotationCB.Items.AddRange(new object[] {
+            "Jour",
+            "Nuit",
+            "Repos 1",
+            "Repos 2"});
+            this.currentRotationCB.Location = new System.Drawing.Point(356, 11);
+            this.currentRotationCB.Name = "currentRotationCB";
+            this.currentRotationCB.Size = new System.Drawing.Size(121, 21);
+            this.currentRotationCB.TabIndex = 3;
+            this.currentRotationCB.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "rotation";
+            // 
+            // endDatePicker
+            // 
+            this.endDatePicker.Location = new System.Drawing.Point(81, 54);
+            this.endDatePicker.Name = "endDatePicker";
+            this.endDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDatePicker.TabIndex = 5;
+            this.endDatePicker.ValueChanged += new System.EventHandler(this.endDatePicker_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Le jour ";
+            // 
+            // rotationTB
+            // 
+            this.rotationTB.Location = new System.Drawing.Point(356, 54);
+            this.rotationTB.Name = "rotationTB";
+            this.rotationTB.Size = new System.Drawing.Size(121, 20);
+            this.rotationTB.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 93);
+            this.Controls.Add(this.rotationTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.endDatePicker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.currentRotationCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startDatePicker);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calendrier Travail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker startDatePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox currentRotationCB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker endDatePicker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox rotationTB;
+    }
+}
+
---
+++ Work/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Work
+{
+    public partial class Form1 : Form
+    {
+        private string[] Rotations = new string[] { "Jour", "Nuit", "Repos 1", "Repos 2" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            startDatePicker.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            endDatePicker.Value = startDatePicker.Value.AddDays(1);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            DateTime startDate = startDatePicker.Value;
+            DateTime endDate = endDatePicker.Value;
+
+            int difference = (endDate - startDate).Days;
+
+            int offset = (difference + currentRotationCB.SelectedIndex) % 4;
+
+            rotationTB.Text = Rotations[offset];
+        }
+
+        private void endDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+            if (currentRotationCB.SelectedIndex > -1) Calculate();
+        }
+    }
+}
